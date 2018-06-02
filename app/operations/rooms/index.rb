86a5b09f1427bf7f985ea8af7eba51127ddc231a
Mykola Kovalchuk <mkovalchuk@gonewfire.com>
2018-06-02 20:15:31 +0300

    Fix search

--- app/operations/rooms/index.rb
+++ app/operations/rooms/index.rb
@@ -13,15 +13,34 @@ class Rooms::Index < Rooms::Base
 
     @base_rooms = BaseRoom.all
 
-    return @base_rooms unless params[:q]
+    return @base_rooms unless query
 
-    if (sq = params[:q].to_f) > 0
-      @base_rooms = @base_rooms.
-        ransack(square_or_number_of_tables_or_number_of_seats_eq: sq).result
+    @base_rooms = @base_rooms.where(id: rooms_by_equality_ids | rooms_by_containing_ids)
+  end
 
-      return @base_rooms if @base_rooms.any?
-    end
+  def search_string
+    return @search_string if defined? @search_string
 
-    @base_rooms = @base_rooms.ransack(name_or_number_start: params[:q]).result
+    @search_string = 0 if query == '0'
+    @search_string = query.to_f if query.to_f > 0
+    @search_string ||= -1
+  end
+
+  def query
+    return @query if defined? @query
+
+    @query = params[:q]
+  end
+
+  def rooms_by_equality_ids
+    return [] if search_string == -1
+
+    @base_rooms.
+      ransack(square_or_number_of_tables_or_number_of_seats_eq: search_string).
+      result.ids
+  end
+
+  def rooms_by_containing_ids
+    @base_rooms.ransack(name_or_number_cont: query).result.ids
   end
 end
